ee3fe4f92746d0c61c498c62a34ccf036433abfe
Afsana Meem <afsumeem@gmail.com>
2023-11-04 12:32:16 +0600

    checkbox toggle added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -11,28 +11,57 @@ const Gallery = () => {
       .then((data) => setImageData(data));
   }, []);
 
+  //
+  const countSelectedImages = () => {
+    return imageData.filter((image) => image.checked).length;
+  };
+  //
+  const handleCheckboxToggle = (id) => {
+    setImageData((prevImageData) =>
+      prevImageData.map((image) =>
+        image.id === id ? { ...image, checked: !image.checked } : image
+      )
+    );
+  };
+
   return (
     <div>
       <header>
-        <h2>Photo Gallery</h2>
+        <h2 style={{ textAlign: "center" }}>Photo Gallery</h2>
       </header>
       <main>
         <div>
+          <div style={{ textAlign: "center" }}>
+            {" "}
+            {countSelectedImages()} image{countSelectedImages() !== 1 && "s"}{" "}
+            selected
+          </div>
           <div className="gallery-container">
             {imageData.length === 0 ? (
               "no image found"
             ) : (
               <div className="image-container">
                 {imageData.map((image) => (
-                  <div key={image.id} className="image-items">
-                    <div className="img-div">
+                  <div
+                    key={image.id}
+                    className={`image-items ${image.checked ? "checked" : ""}`}
+                  >
+                    <div
+                      className="img-div"
+                      onClick={() => handleCheckboxToggle(image.id)}
+                    >
                       <img
                         src={image.image}
                         className="image"
                         alt="gallery-img"
                       />
+                      <input
+                        type="checkbox"
+                        checked={image.checked}
+                        className="image-checkbox"
+                        onChange={() => {}}
+                      />
                     </div>
-                    <input type="checkbox" className="image-checkbox" />
                   </div>
                 ))}
                 <div>
